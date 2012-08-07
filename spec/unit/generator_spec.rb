ca1662eb00b9fe7c3fa59737b18d02b4a66fd43e
Aneta <aneta.barbos@kaeuferportal.de>
2012-08-07 13:37:46 +0200

    add spec_helper

--- spec/unit/generator_spec.rb
+++ spec/unit/generator_spec.rb
@@ -1,11 +1,12 @@
-require 'rspec'
+require 'spec_helper'
+require 'queue_simulator/generator'
 
-describe 'generator' do
+module QueueSimulator
+  describe Generator do
 
-  it 'randomly chooses a number of jobs to add' do
-    number = QueueSimulator::Generator.job_number
-    number.should be < QueueSimulator::Generator::MAX_JOB_NUMBER
+    it 'randomly chooses a number of jobs to add' do
+      number = QueueSimulator::Generator.job_number
+      number.should be < QueueSimulator::Generator::MAX_JOB_NUMBER
+    end
   end
-
-
 end
