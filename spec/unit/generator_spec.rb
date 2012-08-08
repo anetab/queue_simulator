9176e4a13608fdaef96533279f5159916057fee3
Aneta <aneta.barbos@kaeuferportal.de>
2012-08-08 10:21:23 +0200

    change specs for generator

--- spec/unit/generator_spec.rb
+++ spec/unit/generator_spec.rb
@@ -14,11 +14,11 @@ module QueueSimulator
       QueueSimulator::Generator::jobs_to_add(0,5).should eq [1, 2, 3, 4, 5]
     end
 
-    it 'it adds the new jobs to the queue ' do
+    it 'adds the new jobs to the queue ' do
       redis = Redis.new()
       redis.flushdb
-      generator.add_jobs_to_queue("test_queue", [1,2,3,7,9])
-      redis.lrange("test_queue", 0, -1).should eq ["9","7","3","2","1"]
+      generator.add_jobs_to_queue([1,2,3,7,9])
+      redis.lrange("queue1", 0, -1).should eq ["9","7","3","2","1"]
     end
 
     it 'adds jobs to the queue when perform is called' do
