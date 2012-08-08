7e933e833361e0d390ebd6f13464b0fd15f79fe0
Aneta <aneta.barbos@kaeuferportal.de>
2012-08-08 10:22:06 +0200

    refactor processor

--- lib/queue_simulator/processor.rb
+++ lib/queue_simulator/processor.rb
@@ -2,38 +2,36 @@ require 'redis'
 
 module QueueSimulator
 
-  module Processor
-    extend self
+  class Processor
 
     MAX_WORKER_NUMBER = 3
     MAX_SLEEP_TIME = 3
 
     attr_reader :worker_threads
 
-    @queue_key = 'queue1'
-    @random = Random.new()
-    @worker_threads = []
-
-    def number_of_workers
-      if !(@number_of_workers)
-        @number_of_workers = 1 + @random.rand(MAX_WORKER_NUMBER)
-      else
-        @number_of_workers
-      end
+    def initialize(queue_name)
+      @queue_name = queue_name
+      @redis = Redis.new()
+      @worker_threads = []
     end
 
     def join_threads
       worker_threads.map(&:join)
     end
 
+    def remove_job
+      @redis.rpop(@queue_name)
+    end
+
     def perform
       trap("INT") { QueueSimulator.stop}
       @redis = Redis.new()
+      number_of_workers = (1..MAX_WORKER_NUMBER).to_a.sample
       number_of_workers.times do
         @worker_threads << Thread.new do
           until QueueSimulator.stopped? do
-            puts "processed job #{@redis.rpop(@queue_key)}"
-            sleep(@random.rand(MAX_SLEEP_TIME))
+            puts "processed job #{remove_job}"
+            sleep((0..MAX_SLEEP_TIME).to_a.sample)
           end
         end
       end
