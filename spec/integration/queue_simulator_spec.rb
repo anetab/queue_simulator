08a055ae265d9fb2907735482a3b5e13b69134a5
Aneta <aneta.barbos@kaeuferportal.de>
2012-08-08 13:12:46 +0200

    add integration specs

--- spec/integration/queue_simulator_spec.rb
+++ spec/integration/queue_simulator_spec.rb
@@ -7,14 +7,30 @@ describe QueueSimulator do
   let(:processor){QueueSimulator::Processor.new(queue_name)}
 
   before :each do
+    QueueSimulator.stop
+    sleep(1)
+    Redis.new().flushdb
+    QueueSimulator.start
     generator.perform
     processor.perform
-    sleep(10)
+    sleep(1)
+  end
+
+  it 'has generator and processor threads while running' do
+    generator.job_thread.alive?.should be true
+    processor.worker_threads.count.times do |i|
+      processor.worker_threads[i].alive?.should be true
+    end
+
   end
 
   it 'stops the program when interrupt is true' do
     QueueSimulator.stop
     sleep(QueueSimulator::Generator::MAX_SLEEP_TIME+1)
     generator.job_thread.alive?.should be false
+    processor.worker_threads.count.times do |i|
+      processor.worker_threads[i].alive?.should be false
+    end
+
   end
 end
