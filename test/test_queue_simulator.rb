b52394103b69cb9db80333a3e2b4684c2dbed96c
Aneta <aneta.barbos@kaeuferportal.de>
2012-08-08 13:43:08 +0200

    delete test file

--- test/test_queue_simulator.rb
+++
@@ -1,8 +0,0 @@
-require '../lib/queue_simulator/generator'
-require '../lib/queue_simulator/processor'
-
-
-QueueSimulator::Generator.perform
-QueueSimulator::Processor.perform
-QueueSimulator::Generator.join_thread
-QueueSimulator::Processor.join_threads
